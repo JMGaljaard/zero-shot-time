91cab5192c1209b540ee72546e82aff5169155ab
Jeroen M. Galjaard <J.M.Galjaard@tudelft.nl>
2023-12-08 17:34:32 +0100

    Add scikit-learn as a dependency

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ setup(
             'transformers==4.35.2',
             'black==23.11.0',
             'ruff==0.1.6'
+            'scikit-learn==1.3.2'
         ],
         url='https://github.com/JMGaljaard/zero-shot-time',
         license='',
         author='JMGaljaard',
         author_email='J.M.Galjaard@tudelft.nl',
         description='',
-\
 )
