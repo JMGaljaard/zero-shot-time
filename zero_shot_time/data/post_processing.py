6279dfa93a8e615df85d538905a194de4dcc493b
Jeroen M. Galjaard <J.M.Galjaard@tudelft.nl>
2023-12-13 17:01:52 +0100

    Add mapping function to map tokenized data back to numerical representations

---
+++ zero_shot_time/data/post_processing.py
@@ -0,0 +1,66 @@
+import typing as tp
+
+import numpy as np
+import torch
+import transformers
+
+
+def base_transformation(base=10, precision=3, seperator=" "):
+    """Helper function with partial application (currying) to transform an encoded value to a floating point value.
+
+    Args:
+         base (int): Base in which the number is encoded, bases other than 10 are currently not supported.
+         precision (int): Precision of the number to re-construct, i.e. maximum token lenght of an encoded number.
+         seperator (str): 'Bit' seperator used in the encoding scheme, defaults to ' ' as described in original work.
+
+    Returns:
+        floating point number representation of the encoded value.
+
+    """
+    local_precision = precision
+    local_base = float(base)
+    local_seperator = seperator
+
+    if base != 10:
+        raise NotImplementedError("Support for bases other than 10 is not yet implemented!")
+
+    def curried_transform(value: str):
+        # Reverse the order from MSB to LSB
+        value = np.flip(np.array(value.split(local_seperator), dtype=np.int32))
+        D = len(value)
+        # Generate a list of powers of 10 with [10^-1, 10^-2, ..., 10^-precision+1, 10^-precision]
+        powers = np.arange(-local_precision, -local_precision + D)
+        # Calculateteh
+        val = np.sum(value * local_base**powers)
+        return val
+
+    return curried_transform
+
+
+def convert_tokens_to_timeseries(
+    token_ids: torch.LongTensor,
+    tokenizer: transformers.PreTrainedTokenizerFast,
+    mapping_function: tp.Callable = None,
+    delim: str = ", ",
+    dtype=np.float64,
+) -> np.array:
+    """Function to transfrom encoded text back to  tokenized data back to floating points
+
+    Args:
+        token_ids: Predicted token_ids by the language model
+
+
+    """
+
+    # 1. Convert tokens to text
+
+    texts = tokenizer.decode(token_ids)
+
+    # 2. Split values to individual samples
+    string_values = texts.split(delim)
+
+    # 3. Convert values to values
+    # TODO: Properly handle invalid values
+    values = [mapping_function(val) for value in string_values if (val := value.strip()) != ""]
+
+    return np.array(values, dtype=dtype)
