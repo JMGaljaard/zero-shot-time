36eee21e4795ed88425bcab1ac6a22083e8179b9
Jeroen M. Galjaard <J.M.Galjaard@tudelft.nl>
2023-12-14 14:22:52 +0100

    Refactor main file to accomodate baseline execution

--- main.py
+++ main.py
@@ -7,16 +7,27 @@ import transformers
 from zero_shot_time.data import get_dataset, pre_processing
 from zero_shot_time.generation import set_padding_or_none
 
+def main_baseline(args: argparse.Namespace):
+    # TODO: Implement baserunning:
+    pass
 
-def main(
-    model: str = "distilpgt2",
-    dataset: str = "hpc",
-    sub_category: tp.Optional[str] = None,
-    max_history: int = 400,
-    truncation: str = "before",
+def main_llm(
+    args: argparse.Namespace
 ) -> None:
+
+    """Main function to run LLMTime Reimplementation experiments.
+
+    """
+    model = args.model,
+    dataset = args.dataset,
+    sub_category = args.subcat,
+    max_history = args.max_history,
+    truncation = args.truncate,
+
     # 1.1 Load model
-    model: transformers.GPT2Model = transformers.AutoModel.from_pretrained(model)
+    # model: transformers.GPT2Model = transformers.AutoModel.from_pretrained(model)
+    model = transformers.GPT2Model()
+    model.init_weights()
 
     # 1.2.1 Load and initialize the models' tokenizer and prepare tokenizer for batch-encoding plus.
     tokenizer = transformers.GPT2TokenizerFast.from_pretrained("distilgpt2")
@@ -41,36 +52,59 @@ def main(
         logging.warning("Truncating will leak information of previous states")
     print(input_ids)
 
+    warper = NumericalLogitsWarper(
+
+    )
+    outputs = model.generate(
+        inputs=input_ids,
+        logits_processor=warper,
+        max_length=4096
+    )
+
+    # TODO: Post-processing
+    ...
+
+    # TODO: Write to a file
+
+    ...
 
 if __name__ == "__main__":
     default_max_history = 400
-    default_model = "distilgpt2"
+    default_model = "T5"
     default_dataset = "hpc"
     default_subcat = None
     default_truncate = "before"
     # Create argparse parser
     parser = argparse.ArgumentParser(description="Description of your experiment with Zero-Shot-Time")
+    subparsers = parser.add_subparsers(dest='action', help='Available actions to run')
+
+    baseline_parser = subparsers.add_parser('baseline', description='Run baseline experiment')
+
+    baseline_parser.add_argument("--model", type=str, choices=['arima', 'nbeats', 'nhits'], default=default_model,
+                                help=f"Model name (default: {default_model})")
+
+    llmtime_parser = subparsers.add_parser('llmtime', description='Run LLMTime experiment')
 
     # Add command line arguments
-    parser.add_argument(
+    llmtime_parser.add_argument(
         "--max_history",
         type=int,
         default=default_max_history,
         help=f"Maximum history length (default: {default_max_history})",
     )
-    parser.add_argument("--model", type=str, default=default_model, help=f"Model name (default: {default_model})")
-    parser.add_argument(
+    llmtime_parser.add_argument("--model", type=str, default=default_model, help=f"Model name (default: {default_model})")
+    llmtime_parser.add_argument(
         "--dataset", type=str, default=default_dataset, help=f"Dataset name (default: {default_dataset})"
     )
 
-    parser.add_argument(
+    llmtime_parser.add_argument(
         "--subcat",
         type=str,
         default=default_subcat,
         required=False,
         help="Dataset subcategory (needed for HF dataset) (default: None)",
     )
-    parser.add_argument(
+    llmtime_parser.add_argument(
         "--truncate",
         type=str,
         choices=["before", "after"],
@@ -81,10 +115,11 @@ if __name__ == "__main__":
     # Parse command line arguments
     args = parser.parse_args()
 
-    main(
-        model=args.model,
-        dataset=args.dataset,
-        sub_category=args.subcat,
-        max_history=args.max_history,
-        truncation=args.truncate,
-    )
+
+    if args.action == 'baseline':
+        # Run baseline code
+        main_baseline(args)
+    if args.action == 'llmtime':
+        main_llm(
+            args
+        )
