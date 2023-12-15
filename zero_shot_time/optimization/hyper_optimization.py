Change nll estimation to use forward function, and fix computation errors

zero_shot_time/optimization/hyper_optimization.py
@@ -1,10 +1,12 @@
 import copy
 import typing as tp
 
+import jax
 import numpy as np
 import optuna.study
 import torch
 import transformers.models.gpt2.modeling_gpt2
+from optuna import Study
 
 from zero_shot_time.data import pre_processing
 from optuna.samplers import BruteForceSampler
@@ -15,7 +17,7 @@ from zero_shot_time.generation.nll import calculate_negative_log_likelihood
 
 
 def process_sets(
-        param_sets: tp.List[tp.List[np.array], tp.List[np.array]],
+        param_sets: tp.List[tp.Tuple[tp.List[np.array], tp.List[np.array]]],
         precision: int = 3,
         tokenizer: transformers.PreTrainedTokenizerFast = None,
         **scaler_kwargs
@@ -35,26 +37,27 @@ def process_sets(
     """
     train_sets, test_sets = [], []
     hyper_scalers = []
-
     for train_set, test_set in param_sets:
         # 1. Pre-process training and compute scaling object (prevent knowledge leakage through scaling
         scaler, process_values_train, input_ids_train = pre_processing.convert_timeseries_to_fixed_precision(
-            None, tokenizer, values=train_set, precision=precision, **scaler_kwargs
+            None, tokenizer, values=train_set, precision=precision, seperator=' ,', **scaler_kwargs
         )
         # 2. Pre-process test and re-use scaling so tha
         _, process_values_test, input_ids_test = pre_processing.convert_timeseries_to_fixed_precision(
-            None, tokenizer, values=test_set, pre_processor=scaler, precision=precision, **scaler_kwargs
+            None, tokenizer, values=test_set, pre_processor=scaler, precision=precision, seperator=' ,', **scaler_kwargs
         )
 
-        train_sets.append(process_values_train)
-        test_sets.append(process_values_test)
+        # Remove the last comma seperated value, as we are not going to predict futher than that
+        input_ids_test = input_ids_test[:-1]
+
+        train_sets.append(input_ids_train)
+        test_sets.append(input_ids_test)
         hyper_scalers.append(scaler)
 
     return train_sets, test_sets, hyper_scalers
 
 def curried_hyper_opt(
-    train_sets: tp.List[np.array],
-    test_sets: tp.List[np.array],
+    train_eval_sets: tp.List[tp.Tuple[np.array, np.array]],
     model: transformers.PreTrainedModel,
     tokenizer: transformers.PreTrainedTokenizerFast,
     allowable_tokens_mask: torch.BoolTensor,
@@ -69,8 +72,7 @@ def curried_hyper_opt(
     'required', but are presently provided with debugging and extension in mind.
 
     Args:
-        train_sets (List[np.array]): List of arrays with historical data or train sets.
-        test_sets (List[np.array]): List of arrays with 'evaluation' or test sets.
+        train_sets (List[(np.array, np.array)]): List of Tuples consisting fo matching training and testing sets.
         model (): Language models to perform learning with.
         tokenizer (): Tokenizer corresponding to the models.
         allowable_tokens_mask (): Token mask corresponding to the set of allowable numerical tokens, this is required to
@@ -127,41 +129,80 @@ def curried_hyper_opt(
         precision = trial.suggest_categorical('precision',
                                               choices=precision_grid)
 
-        #
-        generation_config = copy.copy(model.generation_config)
-        # Overwrite temperatur of local generation config
-        generation_config.temperature = tau
+
         # 1. Create tokenized representation
 
         # 2. Pre-process and tokenize data
         train_sets_tokens, test_sets_tokens, scalers = process_sets(
-            list(zip(train_sets, test_sets)),
+            train_eval_sets,
             precision=precision,                                # Precision (decimal) to continue with
             tokenizer=tokenizer,                                #
-            alpha=alpha,                                        # Scaler alpha
+            quantile=alpha,                                        # Scaler alpha
             beta=beta,                                          # Scaler beta
         )
 
         # 3. Calculate NLL statistic on pre-processed data.
         nll_aggregate = 0
-        for train_set, test_set in zip(train_sets_tokens, test_sets_tokens):
+        for (train_value, test_value), train_set, test_set in zip(train_eval_sets, train_sets_tokens, test_sets_tokens):
+            # Retrieve grad (scalar) from learned scaler
+            dy_dx = jax.vmap(jax.grad(scalers[0].transform))(
+                np.array(train_value)
+            ).mean().item()
             nll_ = calculate_negative_log_likelihood(
                 model=model,                                    #
                 input_token_ids=train_set,                      # Provide input in token_id representation
                 target_token_ids=test_set,                      # Provide targets in token_id representation
-                seperator_token_id=seperator_token_id,          #
+                separator_token_id=seperator_token_id,          #
                 padding_token_id=padding_token_id,              #
-                generation_config=generation_config,            # Set tau
                 dy_dx=dy_dx,                                    # Conversion from scaled back to 'ordinary' values.
                 token_mask=allowable_tokens_mask,               #
-
+                precision=precision,
+                base=10,
+                temperature=tau
             )
             dy_dx = 1       # TODO: compute from scaler with jax
 
             nll_aggregate += nll_
         # Return the average nll score to the caller for hyper-parameter optimization.
-        return nll_aggregate / len(train_sets)
+        return nll_aggregate / len(train_eval_sets)
 
     # Return curried function to be called with local environment.
     return llm_hyper_opt
 
+
+
+def perform_hyper_parameter_tuning(
+        dataset_name: str,
+        model_name: str,
+        model: transformers.PreTrainedModel,
+        tokenizer: transformers.PreTrainedTokenizerFast,
+        data_sets: tp.List[tp.List[tp.Tuple[np.array, np.array]]],
+        allowable_token_mask: torch.BoolTensor,
+        seperator_token_id: int,
+        padding_token_id: int,
+        search_space: dict[str, tp.List[tp.Any]],
+    ) -> Study:
+    name = f"{dataset_name}_{model_name}"
+    storage = f"sqlite:///{name}.db"
+    study = optuna.create_study(
+        study_name=name,
+        storage=storage,
+        sampler=BruteForceSampler(seed=42),
+        load_if_exists=True
+    )
+    partial_applied_search = curried_hyper_opt(
+        data_sets,
+        model=model,
+        tokenizer=tokenizer,
+        allowable_tokens_mask=allowable_token_mask,
+        seperator_token_id=seperator_token_id,
+        padding_token_id=padding_token_id,
+        alpha=search_space['alpha'],
+        beta=search_space['beta'],
+        precision=search_space['precision'],
+        tau=search_space['tau'],
+    )
+    # Because we peform brute-force search, we don't specify number of trails.
+    study.optimize(partial_applied_search)
+
+    return study